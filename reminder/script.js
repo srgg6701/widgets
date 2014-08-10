52a21346cfb7cce79a98c20d2c2a30c888c387fe
srgg6701 <srgg67@gmail.com>
2014-08-10 23:53:27 +0400

    модификация будильника - теперь выводит колич. оставшегося времени.

--- reminder/script.js
+++ reminder/script.js
@@ -5,15 +5,31 @@ function syncValues(inpToId,inpFrom){
 window.onload=function(){	
 	syncValues('range');
 	var makeDecition = (function (){
-		var tm, 
+		var tm, tm_rest,
 			stp=false,
 			gotm = false,
+            getVal = function(){
+                return document.getElementById('intrvl').value;
+            },
 			sttm = function(cancel){
 				if(cancel) console.log('%ccancel: '+cancel,'color:violet');
-				var min=document.getElementById('intrvl').value;	
-				tm=setTimeout(makeDecition,min*60*1000);
-				//console.log('tm: '+tm+', min: '+min);
-			};			
+				var min=getVal();
+                tm=setTimeout(makeDecition,min*60*1000);
+                tm_rest=min*60;
+                //console.log('tm_rest start: '+tm_rest);
+                var tm2=setInterval(function(){
+                    var info;
+                    tm_rest-=1;
+                    if(tm_rest>=60){
+                        info=((tm_rest/60).toFixed(0)-1) + ' мин. '
+                            + (tm_rest%60);
+                    }else
+                        info=tm_rest;
+                    info+=' сек.';
+                    document.getElementById('tmrest').innerHTML=info;
+                    //console.log('tm_rest? = '+tm_rest);
+                },1000);
+            };
 		return function(command){
 			//console.dir(cancel);
 			if(command===true){
