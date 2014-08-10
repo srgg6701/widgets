04df591a14fdc8e916273d4fdc3be8cd0375d3ef
srgg6701 <srgg67@gmail.com>
2014-08-11 00:29:51 +0400

    Рефакторинг.

--- reminder/script.js
+++ reminder/script.js
@@ -5,54 +5,40 @@ function syncValues(inpToId,inpFrom){
 window.onload=function(){	
 	syncValues('range');
 	var makeDecition = (function (){
-		var tm, tm_rest,
-			stp=false,
-			gotm = false,
-            getVal = function(){
-                return document.getElementById('intrvl').value;
-            },
-			sttm = function(cancel){
-				if(cancel) console.log('%ccancel: '+cancel,'color:violet');
-				var min=getVal();
-                tm=setTimeout(makeDecition,min*60*1000);
+		var tm2, tm_rest, activateButton=function(){
+            document.getElementById('start').disabled=false;
+        };
+		return function(command){
+			if(command===true){
+				clearTimeout(tm2);
+                activateButton();
+			}else{
+                var min=document.getElementById('intrvl').value;
                 tm_rest=min*60;
-                //console.log('tm_rest start: '+tm_rest);
-                var tm2=setInterval(function(){
+                var showTime=function(){
                     var info;
-                    tm_rest-=1;
                     if(tm_rest>=60){
                         info=((tm_rest/60).toFixed(0)-1) + ' мин. '
                             + (tm_rest%60);
-                    }else
+                    }else{
                         info=tm_rest;
+                    }
                     info+=' сек.';
                     document.getElementById('tmrest').innerHTML=info;
-                    //console.log('tm_rest? = '+tm_rest);
-                },1000);
-            };
-		return function(command){
-			//console.dir(cancel);
-			if(command===true){
-				stp=false;
-				gotm=null;
-				clearTimeout(tm);
-			}else{
-				if(gotm){
-					//console.log('gotm = '+gotm+', stp = '+stp);
-					if(!stp){
-						var ntm=new Date();
-						var dt=document.getElementById('stats');
-						var content = dt.innerHTML;
-						dt.innerHTML='<div>'+ntm.toLocaleString()+'</div>'+content;
-						if(!confirm("Продолжить?")) gotm=null;
-					}
-				}else{ //console.log('makeDecition is run once!');
-					stp=false;		
-					gotm=true;						
-				}								
-				if (gotm!==null) sttm();			
+                    if(tm_rest==0){
+                        var ntm=new Date();
+                        var dt=document.getElementById('stats');
+                        var content = dt.innerHTML;
+                        dt.innerHTML='<div>'+ntm.toLocaleString()+'</div>'+content;
+                        clearInterval(tm2);
+                        if(confirm("Продолжить?")) makeDecition();
+                        else activateButton();
+                    }
+                    tm_rest-=1;
+                };
+                showTime();
+                tm2=setInterval(showTime,1000);
 			}
-			if(gotm===null) document.getElementById('start').disabled=false;
 		};
 	}());
 	document.getElementById('start').onclick=function(event){
