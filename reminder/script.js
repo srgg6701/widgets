d880640403c008127939f296d3d58022b6763ef0
srgg6701 <srgg67@gmail.com>
2014-08-11 12:21:39 +0400

    Добавлены элементы поминутного изменения значения таймера.

--- reminder/script.js
+++ reminder/script.js
@@ -1,12 +1,14 @@
+function getTimeCell(){
+    return document.getElementById('intrvl');
+}
 function syncValues(inpToId,inpFrom){
-	if(!inpFrom) inpFrom=document.getElementById('intrvl');
+	if(!inpFrom) inpFrom=getTimeCell();
 	document.getElementById(inpToId).value=inpFrom.value;
 }
 window.onload=function(){	
 	syncValues('range');
 	var callReminder = (function (){
-		var tm, tm_rest,
-            activateButton=function(){
+		var activateButton=function(){
                 document.getElementById('start').disabled=false;
             },
             innerBar=document.querySelector('#rest-bar >div');
@@ -16,17 +18,29 @@ window.onload=function(){
                 activateButton();
 			}else{
                 var min=document.getElementById('intrvl').value;
-                tm_rest=min*60;
+                var tm,tmcount,
+                    tm_rest=min*60,
+                    time_start=tm_rest,
+                    tm_rest_fixed=(tm_rest/60).toFixed(0),
+                    timeBox = document.getElementById('tmrest'),
+                    info,
+                    percentage;
                 var showTime=function(){
-                    var info, percentage;
+                    // если первый запуск
+                    if(time_start){
+                        tmcount=tm_rest_fixed; // отобразить первое значение, как установочное
+                        time_start=null; // удалить метку начала
+                    }else{
+                        tmcount=tm_rest_fixed-1;
+                    }
                     if(tm_rest>=60){
-                        info=((tm_rest/60).toFixed(0)-1) + ' мин. '
+                        info=tmcount + ' мин. '
                             + (tm_rest%60);
                     }else{
                         info=tm_rest;
                     }
                     info+=' сек.';
-                    document.getElementById('tmrest').innerHTML=info;
+                    timeBox.innerHTML=info;
                     percentage = tm_rest/(min*60)*100;
                     innerBar.style.width=percentage+'%';
                     if(tm_rest==0){
@@ -54,4 +68,18 @@ window.onload=function(){
 	};
 	document.getElementById('intrvl').onfocus=function(){callReminder(true);};
 	document.getElementById('range').onfocus=function(){callReminder(true);};
+    var pointers=document.querySelectorAll('#time_less, #time_more');
+    console.dir(pointers);
+    for(var i in pointers){
+        if(pointers[i].hasOwnProperty('innerHTML')) //console.dir(pointers[i]);
+            pointers[i].addEventListener('click', function(event) {
+                var element = event.currentTarget;
+                console.dir(event.currentTarget);
+                var timeCell = getTimeCell();
+                if (element.id == 'time_less') timeCell.value--;
+                else if (element.id == 'time_more') timeCell.value++;
+                syncValues('range');
+            });
+    }
+    document.getElementById('controls').onselectstart=function(){return false;};
 };
